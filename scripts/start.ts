6d35b92b92be24695eb16692b89acb7ed2b20d18
Sayan Naskar <nascarsayan@gmail.com>
2023-01-20 15:45:13 +0530

    nits

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,8 +1,7 @@
 import { GetInstalledBrowsers } from "get-installed-browsers";
 import fs from "fs";
-import { resolve } from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
